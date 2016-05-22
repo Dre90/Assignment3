98405a75add789d69eed8c624ae20ee9dbfb3056
Sjur Sutterud Sagen <sssagen@hotmail.com>
2016-05-22 22:16:45 +0200

    lagt til kommentarer i modellene

--- app/Item.php
+++ app/Item.php
@@ -6,14 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
+    /**
+    * Get the item's category.
+    */
     public function category() {
       return $this->belongsTo(Category::class, 'categoryId');
     }
 
+    /**
+    * Get the item's user.
+    */
     public function user() {
       return $this->belongsTo(User::class, 'userId', 'id');
     }
 
+    /**
+    * Get the item's conversations.
+    */
     public function conversation() {
       return $this->hasMany(Conversation::class, 'itemId', 'id');
     }
--- app/Message.php
+++ app/Message.php
@@ -6,10 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
+  /**
+   * Get the messages' conversation.
+   */
   public function conversation() {
     return $this->belongsTo(Conversation::class, 'conversationId', 'id');
   }
 
+  /**
+   * Get the messages' user.
+   */
   public function user() {
     return $this->belongsTo(User::class, 'userId', 'id');
   }
