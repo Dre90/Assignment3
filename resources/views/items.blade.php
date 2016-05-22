a40b33d4f4102e8519d156148afef0a9d39f922a
Sjur Sutterud Sagen <sssagen@hotmail.com>
2016-05-22 16:00:36 +0200

    basic tables and sidebar, not done

--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -4,35 +4,73 @@
 <div class="container">
     <div class="row">
         <div class="col-md-3"> <!-- sidebar for choice: active items and given away items -->
-          <h1>Sidebar</h1>
+          <ul class="list-group">
+            <li class="list-group-item">
+              <span class="label label-default label-pill pull-right">{{ count($activeItems) }}</span>
+              Active Items
+            </li>
+            <li class="list-group-item">
+              <span class="label label-default label-pill pull-right">{{ count($givenAwayItems) }}</span>
+              Given Away Items
+            </li>
+          </ul>
         </div>
         <div class="col-md-9"><!-- main content div -->
           <div class="row">
-            Count for Active Items this User has:{{ count($activeItems) }}<br>
-            Count for Given Away Items this User has:{{ count($givenAwayItems) }}<br>
-
-            @if ( count($activeItems) !== 0)
-              count not 0 activeItems
-            @elseif ( count($givenAwayItems) !== 0)
-              count not 0 givenAwayItems
-            @else
-              you have no item history
-            @endif
-
-
-
-            <!--
-
-            <div class=col-md-12>
+            <div class=col-md-12><!-- active items div start -->
               <h1>Active Items</h1>
-
-            </div>
-            <div class=col-md-12>
-              <h1>Received Items</h1>
-            </div>
-
-            -->
-
+              @if ( count($activeItems) !== 0)
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Item Name</th>
+                      <th>Give Away</th>
+                      <th>Edit</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($activeItems as $item)
+                    <tr>
+                      <td>{{ $item->title }}</td>
+                      <td>give away</td>
+                      <td>edit</td>
+                      <td>delete</td>
+                    </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+              @else
+                <div class="col-md-12">
+                  <h2>You have no active items.</h2>
+                </div>
+              @endif
+            </div><!-- active items div end -->
+            <div class=col-md-12><!-- given away items div start -->
+              <h1>Given Away Items</h1>
+              @if ( count($givenAwayItems) !== 0)
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Item Name</th>
+                      <th>Re-list Item</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($givenAwayItems as $item)
+                    <tr>
+                      <td>{{ $item->title }}</td>
+                      <td>re-list item</td>
+                    </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+              @else
+                <div class="col-md-12">
+                  <h2>You have no given away items.</h2>
+                </div>
+              @endif
+            </div><!-- given away items div end -->
           </div>
         </div>
     </div>
