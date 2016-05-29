a03d92bf4f550fbdab0b9cb3998abf7323d23cfa
Dag-Roger <dag.roger.e@gmail.com>
2016-05-29 16:06:22 +0200

    Kan laste item med bilde

--- app/Http/Controllers/AddItemController.php
+++ app/Http/Controllers/AddItemController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Item;
+use Image;
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+
 
 class AddItemController extends Controller
 {
@@ -25,6 +30,32 @@ class AddItemController extends Controller
      */
     public function index()
     {
-        return view('add_item');
+        $categories = Category::all();
+        return view('add_item',compact('categories'));
+    }
+
+    public function store(Request $request)
+    {
+        if($request->hasFile("image")) {
+            $image = $request->file("image");
+            $filename = time() . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->save( public_path("/resources/item_images/" . $filename) );
+
+
+        }
+
+        $todays_date = date("Y-m-d H:i:s");
+        $new_item = new Item;
+        $new_item->title = $request->title;
+        $new_item->categoryid = $request->category;
+        $new_item->description = $request->description;
+        $new_item->itemImage = $filename;
+        $new_item->userId = $request->_userid;
+        $new_item->created_at = $todays_date;
+        $new_item->updated_at = $todays_date;
+
+        $new_item->save();
+
+        return back();
     }
 }
