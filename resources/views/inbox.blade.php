fef0b1336f6e52cbd0d454311b2801cecd3705b4
Dag-Roger <dag.roger.e@gmail.com>
2016-06-01 20:10:50 +0200

    default bilde lagt til. styling

--- resources/views/inbox.blade.php
+++ resources/views/inbox.blade.php
@@ -3,51 +3,45 @@
 @section('content')
 <div class="container">
     <div class="row">
-      @if(count($conversations) === 0)
-        <div class="col-md-12 text-center">
-          <h1>You do not have any conversations.</h1>
-        </div>
-      @else
-        <div class="col-md-12">
-          <!-- all conversations -->
-          <h1 class="text-center">Your Conversations</h1>
-          <div class="row">
+        @if(count($conversations) === 0)
+            <div class="col-md-12 text-center">
+                <h1>You do not have any conversations.</h1>
+            </div>
+        @else
+            <div class="col-md-12">
+                <!-- all conversations -->
+                <h1 class="text-center">Your Conversations</h1>
+                <ul class="list-group">
+                    @foreach ($conversations as $conversation)
+                        <a href="{{ url('inbox/' . $conversation->id) }}">
+                            <li class="list-group-item conversation-box">
+                                {{-- <h4>Item</h4> --}}
+                                {{ $conversation->item->title }}
 
+                                <!-- if user is owner, show You here -->
 
-            @foreach ($conversations as $conversation)
-                <div class="col-md-4">
-              <form action="{{ url('inbox/' . $conversation->id) }}" method="GET"><!-- Give Away item button -->
-                <button type="submit" id="show-conversation-{{ $conversation->id }}" class="btn btn-default pull-left">
-                            <img src="{{url('resources/item_images/')}}/{{$conversation->item->itemImage }}" class="small-item-image img-thumbnail" alt="profile picture" />
-                            <p>Item: {{ $conversation->item->title }}</p>
+                                  @if($conversation->ownerUser->id === Auth::user()->id)
+                                    {{-- You.</p> --}}
+                                  @else
+                                      <h4>Owner</h4>
+                                    <p>{{ $conversation->ownerUser->name }}</p>
+                                  @endif
 
-                            <!-- if user is owner, show You here -->
-
-                              @if($conversation->ownerUser->id === Auth::user()->id)
-                                {{-- You.</p> --}}
-                              @else
-                                <p>Owner: {{ $conversation->ownerUser->name }}</p>
-                              @endif
-
-                              <!-- if user is interestedUser, show You here -->
-                              @if($conversation->interestedUser->id === Auth::user()->id)
-                                {{-- You.</p> --}}
-                              @else
-                                <p>Interested: {{ $conversation->interestedUser->name }}</p>
-                              @endif
-                        <p>
-                            {{ $conversation->updated_at }}
-                        </p>
-                </button>
-              </form>
-          </div>
-            @endforeach
-
-        </div>
-
-
-    </div>
-      @endif
+                                  <!-- if user is interestedUser, show You here -->
+                                  @if($conversation->interestedUser->id === Auth::user()->id)
+                                    {{-- You.</p> --}}
+                                  @else
+                                      <h4>Interested</h4>
+                                    <p>{{ $conversation->interestedUser->name }}</p>
+                                  @endif
+                                  <strong>Last received message</strong>
+                                  <p>{{ $conversation->updated_at }}</p>
+                              </li>
+                          </a>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div>
 </div>
 @endsection
