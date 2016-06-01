a521d3804e524deddbb8c9335d5182c34bacb6d8
Dag-Roger <dag.roger.e@gmail.com>
2016-06-01 11:38:05 +0200

    Styling

--- resources/views/conversation.blade.php
+++ resources/views/conversation.blade.php
@@ -4,19 +4,34 @@
 <div class="container">
   <div class="row">
     <div class="col-md-12">
-      <h1>Showing conversation for Item: {{ $conversation->item->title }}.</h1>
-      <!-- show owner -->
-      @if($conversation->ownerUser->id === Auth::user()->id)
-        <h2>You are the owner of this item.</h2>
-      @else
-        <h2>{{ $conversation->ownerUser->name }} is the owner of this item.</h2>
-      @endif
-      <!-- show interested user -->
-      @if($conversation->interestedUser->id === Auth::user()->id)
-        <h2>You are interested in this item.</h2>
-      @else
-        <h2>{{ $conversation->interestedUser->name }} is interested in this item.</h2>
-      @endif
+        <div class="row">
+            <div class="col-md-4">
+
+                <h2><a href="{{url('item/')}}/{{$conversation->item->id}}"> {{ $conversation->item->title }}</a></h2>
+            </div>
+
+            <div class="col-md-6 text-right">
+                <!-- show owner -->
+                @if($conversation->ownerUser->id === Auth::user()->id)
+
+                @else
+                  <h4> {{ $conversation->ownerUser->name }} is the owner of this item.</h4>
+                @endif
+                <!-- show interested user -->
+                @if($conversation->interestedUser->id === Auth::user()->id)
+
+                @else
+                  <h4> {{ $conversation->interestedUser->name }} is interested in this item</h4>
+                @endif
+            </div>
+            <div class="col-md-2">
+
+
+            </div>
+        </div>
+      <h1></h1>
+
+
       <h3>Messages</h3>
       <!-- showing messages with correct To and From users -->
       @foreach ($conversationMess as $conversation)
@@ -24,9 +39,10 @@
           @if($message->userId === Auth::user()->id)
             <div class="row conversationMessageCurrentUser">
               <div class="col-md-12">
-                <div class="row">
-                  <p class="col-md-2 text-right"><b>You: </b></p>
-                  <p class="col-md-9">{{ $message->body }}</p>
+                <div class="row ">
+                <p class="col-md-3"></p>
+                  <p class="col-md-7 MessageCurrentUser"><small class="message-dato">{{ $message->created_at }}</small>{{ $message->body }}</p>
+                  <p class="col-md-2"><img src="{{url('resources/user_images/')}}/{{$conversation->interestedUser->userImage }}" class="small-profilePicture img-circle" alt="profile picture" />You</p>
                 </div>
               </div>
             </div>
@@ -34,42 +50,52 @@
             <div class="row conversationMessageOtherUser">
               <div class="col-md-12">
                 <div class="row">
-                  <p class="col-md-2 text-right"><b>{{$conversation->interestedUser->name}}: </b></p>
-                  <p class="col-md-9">{{ $message->body }}</p>
+                  <p class="col-md-2 text-right"><img src="{{url('resources/user_images/')}}/{{$conversation->interestedUser->userImage }}" class="small-profilePicture img-circle" alt="profile picture" />{{$conversation->interestedUser->name}}</p>
+                  <p class="col-md-7 MessageOtherUser"><small class="message-dato">{{ $message->created_at }}</small>{{ $message->body }}</p>
+                  <p class="col-md-3"></p>
                 </div>
               </div>
             </div>
           @elseif($conversation->ownerUser->id === $message->userId)
             <div class="row conversationMessageOtherUser">
-              <div class="col-md-12">
+              <div class="col-md-12 ">
                 <div class="row">
-                  <p class="col-md-2 text-right"><b>{{$conversation->ownerUser->name}}: </b></p>
-                  <p class="col-md-9">{{ $message->body }}</p>
+                  <p class="col-md-2 text-right"><img src="{{url('resources/user_images/')}}/{{$conversation->interestedUser->userImage }}" class="small-profilePicture img-circle" alt="profile picture" />{{$conversation->ownerUser->name}}</p>
+                  <p class="col-md-7 MessageOtherUser"><small class="message-dato">{{ $message->created_at }}</small>{{ $message->body }}</p>
+                  <p class="col-md-3"></p>
                 </div>
               </div>
             </div>
           @endif
         @endforeach
       @endforeach
-      <!-- input form for new message -->
-      <form method="POST" action="{{ url('inbox/' . $conversation->id) }}">
-        {{ csrf_field() }}
-        <input type="hidden" name="convId" value="{{ $conversation->id }}">
-        <input type="hidden" name="userId" value="{{ Auth::user()->id }}">
-        <div class="form-group{{ $errors->has('newMessage') ? ' has-error' : '' }}">
-          <textarea class="form-control message" name="newMessage" value="{{ old('newMessage') }}" rows="4"></textarea>
-          @if ($errors->has('newMessage'))
-            <span class="help-block">
-              <strong>{{ $errors->first('newMessage') }}</strong>
-            </span>
-          @endif
-        </div>
-        <div class="form-group">
-          <div class="col-md-12 text-right">
-            <button type="submit" class="btn btn-primary">Reply</button>
+      <div class="row">
+          <div class="col-md-2 "></div>
+          <div class="col-md-8">
+
+
+          <!-- input form for new message -->
+          <form method="POST" action="{{ url('inbox/' . $conversation->id) }}">
+            {{ csrf_field() }}
+            <input type="hidden" name="convId" value="{{ $conversation->id }}">
+            <input type="hidden" name="userId" value="{{ Auth::user()->id }}">
+            <div class="form-group{{ $errors->has('newMessage') ? ' has-error' : '' }}">
+              <textarea class="form-control message" name="newMessage" value="{{ old('newMessage') }}" rows="4"></textarea>
+              @if ($errors->has('newMessage'))
+                <span class="help-block">
+                  <strong>{{ $errors->first('newMessage') }}</strong>
+                </span>
+              @endif
+            </div>
+            <div class="form-group">
+              <div class="col-md-12 text-right">
+                <button type="submit" class="btn btn-primary">Reply</button>
+              </div>
+            </div>
+          </form>
           </div>
-        </div>
-      </form>
+          <div class="col-md-2 "></div>
+      </div>
     </div>
   </div>
 </div>
